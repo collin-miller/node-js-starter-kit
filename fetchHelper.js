83e1c42c4fef1276ab970ca3486174723929df4d
collin-miller <colljmiller@gmail.com>
2019-09-15 17:01:04 -0400

    feat(logger): added simple loggers to the template

--- fetchHelper.js
+++ fetchHelper.js
@@ -1,8 +1,10 @@
+import logger from './logger';
 export default class FetchHelper {
     constructor(...options) {
         this.options = options[0];
     }
     send() {
+        logger.info(this.options.application)
         return this.options;
     }
 }
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
 import FetchHelper from './fetchHelper';
+import logger from './logger';
 
 const app = express()
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms', { stream: logger.stream }))
 
 app.get('/:application/:environment?/:account?', (req, res) => {
     const fetchHelper = new FetchHelper(req.params);
@@ -9,5 +13,5 @@ app.get('/:application/:environment?/:account?', (req, res) => {
 })
 
 app.listen(8080, () => {
-    console.log("App listening on port 8080")
+    logger.info("App listening on port 8080")
 })
